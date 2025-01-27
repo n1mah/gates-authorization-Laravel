95487cefcb5d88618dc032605061d183601ebc5e
Nima Heidari <nima.heidari.n@gmail.com>
2025-01-28 01:49:07 +0330

    Gate For User Actions Added

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,20 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('user-create', function ($user) {
+            return $user->role == 'admin';
+        });
+        Gate::define('user-update', function ($user,$currentUser) {
+           return ($user->id == $currentUser->id) || ($user->role == 'admin' || $user->role == 'operator');
+        });
+        Gate::define('user-delete', function ($user,$currentUser) {
+           return ($user->id == $currentUser->id) || ($user->role == 'admin' || $user->role == 'operator');
+        });
+        Gate::define('user-change-role', function ($user) {
+           return $user->role == 'admin' || $user->role == 'operator';
+        });
+        Gate::define('user-show', function ($user,$currentUser) {
+            return ($user->id === $currentUser->id) || ($user->role == 'admin' || $user->role == 'operator');
+        });
     }
 }
